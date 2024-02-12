22ca59cb42a030a63865d447ddd109e3f0b6e86c
Davyd Tovstyj <122681484+davydtovstyj@users.noreply.github.com>
2024-02-12 22:55:14 +0200

    Cleaning

--- Simple Calculator/MainWindow.xaml.cs
+++ Simple Calculator/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Simple_Calculator
 {
@@ -16,9 +9,6 @@ namespace Simple_Calculator
         public MainWindow()
         {
             InitializeComponent();
-#if DEBUG
-            NameL.Content += "(dbg)";
-#endif
         }
 
         private void Cross_Click(object sender, RoutedEventArgs e)
